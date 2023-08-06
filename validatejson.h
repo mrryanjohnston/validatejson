3329c917258c0dbe3068a7036a99b88cf4715d41
Ryan Johnston <mrryanjohnston@gmail.com>
2023-08-05 22:35:09 -0400

    Add function signature to header file

--- validatejson.h
+++ validatejson.h
@@ -1,9 +1,13 @@
 #include <stdbool.h>
 
-bool validateJSON(const char *);
-bool validateJSONElement(const char *, int *, int);
 bool validateArray(const char *, int *, int);
 bool validateBoolean(const char *, int *, int);
+bool validateExponent(const char *, int *, int);
+bool validateFraction(const char *, int *, int);
+bool validateJSON(const char *);
+bool validateJSONElement(const char *, int *, int);
+bool validateJSONString(const char *, int *, int);
+bool validateNegative(const char *, int *, int);
 bool validateNumber(const char *, int *, int);
 bool validateObject(const char *, int *, int);
 bool validateString(const char *, int *, int);
