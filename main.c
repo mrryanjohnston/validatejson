9f6c9393408d0b117109995c673fc7803412fb45
Ryan Johnston <mrryanjohnston@gmail.com>
2023-08-31 23:29:21 -0400

    indentation

--- main.c
+++ main.c
@@ -4,8 +4,8 @@
 int main(int argc, char *argv[])
 {
   if (argc == 1) {
-      printf("USAGE\nvalidatejson checks whether the argument passed is a valid string of JSON\n\nExample:\n\tvalidatejson '{ \"foo\": [ 1, 2, \"Bar!\" ] }'\n");
-      return 0;
+    printf("USAGE\nvalidatejson checks whether the argument passed is a valid string of JSON\n\nExample:\n\tvalidatejson '{ \"foo\": [ 1, 2, \"Bar!\" ] }'\n");
+    return 0;
   } else if (argc == 2) {
     if (!validateJSON(argv[1]))
     {
